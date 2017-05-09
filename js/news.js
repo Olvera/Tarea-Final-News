3f173476539446b64ab29ea0044f9590c8e23087
Olvera <f.j.olvera@gmail.com>
2017-05-09 20:08:13 +0200

    Con pub y noticias HTML

--- js/news.js
+++ js/news.js
@@ -16,7 +16,7 @@ function ponerComunidades(json){
      $.each( json, function( i, comunidad ) {
          //dos formas de hacer lo mismo, la segunda es más adecuada.
          //$("#comunidades").append( "<option value='" + comunidad.slug + "'>" + comunidad.comunidad + "</option>" );
-         $('#comunidades').append($('<option>', { value: comunidad.slug, text : comunidad.comunidad }));
+         $('#comunidades').append($('<option>', { value: 1.slug, text : 1.descripcion }));
          //"slug" es la parte de la url, por si hay que visitar la página de la comunidad (no va con vuestro proyecto, es un ejemplo)
      }); 
 }
