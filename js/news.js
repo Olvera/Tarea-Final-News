6da11348f820901a4f4e08924cf46902962d70fe
Olvera <f.j.olvera@gmail.com>
2017-05-11 15:46:40 +0200

    añadido vídeo en news1.html

--- js/news.js
+++ js/news.js
@@ -1,23 +1,15 @@
+var newsJSON=["data/1.json", "data/2.json"]
 $(function(){
     //cargar comunidades
     $.getJSON( "data/1.json", function( jsonObject ) {
-        ponerComunidades( jsonObject );
-    });
-
-    //onclick en verinfo
-    $("#verinfo").click(function() {
-        var v=$("#text").val();
-        if (v!=0) window.location = "/text/"+$("#text").val();
-        else alert('Selecciona una comunidad');
+        //onclick en verinfo
+        $("button").on('click',function() {
+        ponerNews(jsonObject);});
     });
 });
 
-function ponerComunidades(json){
-     $.each( json, function( i, comunidad ) {
-         //dos formas de hacer lo mismo, la segunda es más adecuada.
-         //$("#comunidades").append( "<option value='" + comunidad.slug + "'>" + comunidad.comunidad + "</option>" );
-         $('#comunidades').append($('<option>', { value: 1.slug, text : 1.descripcion }));
-         //"slug" es la parte de la url, por si hay que visitar la página de la comunidad (no va con vuestro proyecto, es un ejemplo)
-     }); 
+function ponerNews(json){
+    $.each( json, function( i, item ) {
+        $('#newNews').append('<div class="col-sm-8"><br><p id="datetime">'+this.datetime+'</p><h3 id="titulo">'+this.titulo'</h3><img id="imagen" src="'+this.imagen+'" class="img-responsive center-block" alt=""><br><p id="descripcion">'+this.descripcion+'</p><br><br></div>')
+    }); 
 }
-    
