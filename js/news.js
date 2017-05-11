c32a3b16ce5453fbf9f22d1597bdc31126ec998b
Olvera <f.j.olvera@gmail.com>
2017-05-11 18:28:04 +0200

    No hay manera con el JSON

--- js/news.js
+++ js/news.js
@@ -1,15 +1,12 @@
-var newsJSON=["data/1.json", "data/2.json"]
 $(function(){
-    //cargar comunidades
     $.getJSON( "data/1.json", function( jsonObject ) {
-        //onclick en verinfo
-        $("button").on('click',function() {
-        ponerNews(jsonObject);});
+        $("#masNot").on('click',function() {
+        ponerNews(jsonObject)});
     });
 });
 
 function ponerNews(json){
     $.each( json, function( i, item ) {
-        $('#newNews').append('<div class="col-sm-8"><br><p id="datetime">'+this.datetime+'</p><h3 id="titulo">'+this.titulo'</h3><img id="imagen" src="'+this.imagen+'" class="img-responsive center-block" alt=""><br><p id="descripcion">'+this.descripcion+'</p><br><br></div>')
+        $("#newNews").append("<div class='col-sm-8'><br><p id='datetime'>"+item.datetime+"</p><h3 id='titulo'>"+item.titulo2+"</h3><img id='imagen' src="+item.imagen+"class='img-responsive center-block' alt=''><br><p id='descripcion'>"+item.descripcion+"</p><br><br></div>")
     }); 
 }
